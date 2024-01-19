5e45eb4c8d66eaa49212df9f82ed9d14ab991082
Alejandro L. Herrero Conesa <57948901+AlejandroLuisHC@users.noreply.github.com>
2024-01-19 18:25:13 +0100

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube ad-blocker
 // @icon         https://www.gstatic.com/youtube/img/branding/favicon/favicon_192x192.png
-// @version      1.0.0
+// @version      1.1
 // @description  Removes ads from YouTube videos and pages by using Enhancer for YouTube's 'Remove Ads' button.
 // @author       AlejandroLHC
 // @updateURL    https://github.com/AlejandroLuisHC/yt-adblocker-script/raw/main/script.user.js
@@ -12,66 +12,58 @@
 (function () {
     'use strict';
 
-    var searchInterval = 100
-    var failCounter = 0;
-    var masterSwitch = true;
+    const searchInterval = 50;
+    let failCounter = 0;
+    let masterSwitch = true;
 
     function removeAds() {
         const currentURL = window.location.href;
-        if (masterSwitch) {
-            if (/https:\/\/www\.youtube\.com\/watch\?.*/.test(currentURL)) {
-                const adShowing = document.querySelector('.ad-showing');
-                const bannerShowing = document.querySelector('#banner');
-                const playerAdsShowing = document.querySelector('#player-ads');
-                const button = document.querySelector('#efyt-not-interested');
 
-                if (bannerShowing) {
-                    bannerShowing.remove();
+        if (!masterSwitch || !currentURL.includes("youtube.com")) {
+            return;
+        }
+
+        if (/https:\/\/www\.youtube\.com\/watch\?.*/.test(currentURL)) {
+            const adShowing = document.querySelector('.ad-showing');
+            const bannerShowing = document.querySelector('#banner');
+            const playerAdsShowing = document.querySelector('#player-ads');
+            const button = document.querySelector('#efyt-not-interested');
+
+            bannerShowing?.remove();
+            playerAdsShowing?.remove();
+
+            if (adShowing) {
+                if (button) {
+                    button.click();
+                    failCounter = 0;
+                } else {
+                    console.error(`Failed to find button. Retrying in ${searchInterval} ms`);
+                    failCounter++;
                 }
 
-                if (playerAdsShowing) {
-                    playerAdsShowing.remove();
-                }
+                if (failCounter > 10) {
+                    const buttonNotFound = window.confirm("Failed to find the 'Remove Ads' button. Please make sure that Enhancer for YouTube is installed.\n\nPress 'OK' to redirect to the installation page.\nPress 'Cancel' to disable the Bypasser for this session.");
 
-                if (adShowing) {
-                    if (button) {
-                        button.click();
+                    if (buttonNotFound) {
+                        window.open("https://chrome.google.com/webstore/detail/enhancer-for-youtube/ponfpcnoihfmfllpaingbgckeeldkhle");
                         failCounter = 0;
-                    }
-                    else {
-                        console.error("Failed to find button. Retrying in ", searchInterval, " ms");
-                        failCounter = failCounter + 1;
-                    }
-                    if (failCounter > 10) {
-                        var buttonNotFound = window.confirm("Failed to find the 'Remove Ads' button. Please make sure that Enhancer for YouTube is installed.\n\nPress 'OK' to redirect to the installation page.\nPress 'Cancel' to disable the Bypasser for this session.");
-                        if (buttonNotFound) {
-                            window.open("https://chrome.google.com/webstore/detail/enhancer-for-youtube/ponfpcnoihfmfllpaingbgckeeldkhle");
-                            failCounter = 0;
-                        } else {
-                            masterSwitch = false;
-                        }
+                    } else {
+                        masterSwitch = false;
                     }
                 }
             }
+        }
 
-            if (/https:\/\/www\.youtube\.com\/.*/.test(currentURL)) {
-                const headAdShowing = document.querySelector('#masthead-ad');
-                const adCardShowing = document.querySelector('ytd-ad-slot-renderer')
-                const ytAdBanner = document.querySelector('ytd-statement-banner-renderer');
+        if (/https:\/\/www\.youtube\.com\/.*/.test(currentURL)) {
+            const headAdShowing = document.querySelector('#masthead-ad');
+            const adCardShowing = document.querySelector('ytd-ad-slot-renderer');
+            const ytAdBanner = document.querySelector('ytd-statement-banner-renderer');
 
-                if (headAdShowing) {
-                    headAdShowing.remove();
-                }
+            headAdShowing?.remove();
 
-                if (adCardShowing) {
-                    const adParent = adCardShowing.parentNode.parentNode;
-                    adParent.remove();
-                }
-
-                if (ytAdBanner) {
-                    const adParent = ytAdBanner.parentNode.parentNode;
-                    adParent.remove();
-                }
+            if (adCardShowing || ytAdBanner) {
+                const adParent = (adCardShowing || ytAdBanner).parentNode.parentNode;
+                adParent?.remove();
             }
         }
     }
