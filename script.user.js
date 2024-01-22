4bf7e97f1b168249e12ddd077c41e763af9f33ab
Alejandro L. Herrero Conesa <57948901+AlejandroLuisHC@users.noreply.github.com>
2024-01-22 11:19:56 +0100

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Ad-blocker
 // @icon         https://www.gstatic.com/youtube/img/branding/favicon/favicon_192x192.png
-// @version      1.3.2
+// @version      1.4.0
 // @description  Removes ads from YouTube videos and pages using Enhancer for YouTube's 'Remove Ads' button.
 // @author       AlejandroLHC
 // @updateURL    https://github.com/AlejandroLuisHC/yt-adblocker-script/raw/main/script.user.js
@@ -31,10 +31,12 @@
             const playerAdsShowing = document.querySelector('#player-ads');
             const skipButtonShowing = document.querySelector('#ytp-ad-skip-button-modern');
             const blockMessageShown = document.querySelector('ytd-enforcement-message-view-model');
+            const premiumDialogShowing = document.querySelector('.mealbar-promo-renderer');
             const button = document.querySelector('#efyt-not-interested');
 
             removeElement(bannerShowing);
             removeElement(playerAdsShowing);
+            removeElement(premiumDialogShowing.parentNode);
             clickElement(skipButtonShowing);
 
             if (blockMessageShown) {
